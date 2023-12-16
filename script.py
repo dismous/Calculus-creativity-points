f8f936fb79a1bd5c6c486b3ff7a0d09c7681cbf2
dismous <bohdanpopov9@gmail.com>
2023-12-16 18:37:14 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,9 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.misc import derivative
 import sympy as sp
+from sympy.parsing.sympy_parser import parse_expr
 
-
-# Define a few functions to use
 def exp(x):
     return np.exp(x)
 
@@ -24,53 +23,64 @@ def tg(x):
 def ctg(x):
     return 1 / np.tan(x)
 
-# Taylor series function
-def taylor_series(function, a, n, x):
-    taylor_sum = np.zeros_like(x)
+def taylor_series(function, a, n):
+    x = sp.symbols('x')
+    taylor_sum = 0
     for i in range(n):
-        term = derivative(function, a, dx=1e-6, n=i, order=2*i+1) / np.math.factorial(i) * (x - a)**i
+        term = function.diff(x, i).subs(x, a) / np.math.factorial(i) * (x - a)**i
         taylor_sum += term
-    return taylor_sum
+    return sp.lambdify(x, taylor_sum, 'numpy')
 
-# Function to plot the Taylor series
+window = tk.Tk()
+window.title("Equation Solver")
+equation = tk.StringVar()
+a_value = tk.StringVar()
+n_value = tk.StringVar()
+equation_label = tk.Label(window, text="Enter equation:")
+equation_label.pack()
+equation_entry = tk.Entry(window, textvariable=equation)
+equation_entry.pack()
+
+a_label = tk.Label(window, text="Enter the point around which the series is expanded:")
+a_label.pack()
+a_entry = tk.Entry(window, textvariable=a_value)
+a_entry.pack()
+
+n_label = tk.Label(window, text="Enter the number of terms in the series:")
+n_label.pack()
+n_entry = tk.Entry(window, textvariable=n_value)
+n_entry.pack()
+
+
+
 def plot_taylor_series(func, a, n):
-    x = np.linspace(-2, 2, 400)
-    taylor_approx = taylor_series(func, a, n, x)
-
+    x = sp.symbols('x')
+    taylor_approx = taylor_series(func, a, n)
+    x_values = np.linspace(a - 5, a + 5, 500)
+    func_values = np.array([func.evalf(subs={x: val}) for val in x_values])
+    taylor_values = taylor_approx(x_values)
     plt.figure(figsize=(10, 6))
-    plt.plot(x, func(x), label=f"{func.__name__}")
-    plt.plot(x, taylor_approx, label=f"Taylor Series - {n} terms")
-    plt.title(f"Taylor Series Approximation of {func.__name__}")
-    plt.xlabel("x")
-    plt.ylabel("y")
+    plt.plot(x_values, func_values, label='Function')
+    plt.plot(x_values, taylor_values, label='Taylor series approximation')
+    plt.scatter([a], [func.evalf(subs={x: a})], color='red')  # point of expansion
     plt.legend()
     plt.grid(True)
     plt.show()
 
-# Create a new window
-window = tk.Tk()
-window.title("Equation Solver")
-
-# Create a StringVar to hold the equation
-equation = tk.StringVar()
-
-# Create an entry field for the equation
-equation_entry = tk.Entry(window, textvariable=equation)
-equation_entry.pack()
-
-# Function to solve the equation
 def solve_equation():
     func_str = equation.get().replace('e^', 'exp(').replace('e**', 'exp(').replace('ln', 'log')
     if func_str.endswith(')') is False and 'exp(' in func_str:
         func_str += ')'
     x = sp.symbols('x')
-    func = sp.lambdify(x, sp.sympify(func_str), 'numpy')
+    func = sp.sympify(func_str)
+    
+    # Get the point around which the series is expanded and the number of terms from the user
+    a = float(a_value.get())
+    n = int(n_value.get())
+    
     window.destroy()
-    plot_taylor_series(func, 0, 5)
+    plot_taylor_series(func, a, n)
 
-# Create a button to solve the equation
 solve_button = tk.Button(window, text="Solve", command=solve_equation)
 solve_button.pack()
-
-# Run the GUI
 window.mainloop()
