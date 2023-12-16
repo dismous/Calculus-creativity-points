13d6461d2a848a1be8c4f8a9d9534eb98e61a9d5
dismous <bohdanpopov9@gmail.com>
2023-12-16 22:28:04 +0200

    dasdsa

--- make_config.py
+++ make_config.py
@@ -4,6 +4,8 @@ main
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel
 from taylor_calculator_and_plotter import *
 from PyQt5.QtGui import QIcon
+from sympy import sympify
+from sympy import *
 
 class MyApp(QWidget):
     def __init__(self):
@@ -13,8 +15,6 @@ class MyApp(QWidget):
         self.development_point_entry = QLineEdit(self)
         self.grade_entry = QLineEdit(self)
         self.plot_range_entry = QLineEdit(self)
-        
-
         self.calculate_button = QPushButton('Calculate and Plot', self)
         self.calculate_button.clicked.connect(self.calculate_and_plot_wrapper)
         self.setWindowIcon(QIcon('images/images.jpg'))
@@ -30,11 +30,14 @@ class MyApp(QWidget):
         layout.addWidget(self.calculate_button)
 
     def calculate_and_plot_wrapper(self):
-        BASE_EXPR = sympify(self.base_expr_entry.text() or 'cos(x)')
+        function_mapping = {'tg': 'tan', 'ctg': '1/tan', 'arctg': 'atan', 'arcctg': 'acotan', 'arcsin': 'asin'}
+        user_input = self.base_expr_entry.text() or 'cos(x)'
+        for user_func in sorted(function_mapping.keys(), key=len, reverse=True):
+            user_input = user_input.replace(user_func, function_mapping[user_func])
+        BASE_EXPR = sympify(user_input)
         DEVELOPMENT_POINT = float(self.development_point_entry.text() or '0')
         GRADE = int(self.grade_entry.text() or '4')
         PLOT_RANGE = int(self.plot_range_entry.text() or '3')
-
         calculate_and_plot(base_expr=BASE_EXPR, development_point=DEVELOPMENT_POINT, grade=GRADE, plot_range=PLOT_RANGE)
 
 app = QApplication([])
--- taylor_calculator_and_plotter.py
+++ taylor_calculator_and_plotter.py
@@ -17,11 +17,8 @@ def calculate_and_plot(development_point: float = 8,
                        base_expr=cos(x),
                        plot_range: int = 4,
                        draw_x_axis: bool = True):
-    # calc ranges for plotting
     plot_min_x, plot_max_x = development_point - plot_range, development_point + plot_range
     plot_points = 1000
-
-    # calculate the nth taylor series for the function (BASE_EXPR)
     base_taylor_expr = Float(lambdify(x, base_expr)(development_point))
     current_function = base_expr
 
@@ -33,39 +30,29 @@ def calculate_and_plot(development_point: float = 8,
 
         current_function = derivative_k
 
-    # print resulting taylor expression
     print('f(x) = ' + str(base_expr))
     print(f'T({grade}, {development_point})(x) = ' + str(base_taylor_expr))
 
-    # and plot both expressions (base_expr and base_taylor_expr)
-    # by first making functions out of the two expressions,
-    # then calculating in an linear space N points and plotting those points with matplotlib
     lam_base_expr = lambdify(x, base_expr, modules=['numpy'])
     lam_taylor_expr = lambdify(x, base_taylor_expr, modules=['numpy'])
 
     x_vals = np.linspace(plot_min_x, plot_max_x, plot_points)
     y_vals_base_expr, y_vals_taylor_expr = lam_base_expr(x_vals), lam_taylor_expr(x_vals)
 
-    # create subplot
     fig, ax = plt.subplots()
-    # plot graphs
     ax.plot(x_vals, y_vals_base_expr, label=str(base_expr))
     ax.plot(x_vals, y_vals_taylor_expr, label=f'T({grade}, {development_point})(x)')
-    # styling (x and y axis, etc.)
     ax.grid(True, which='both')
 
     if plot_min_x < 0 < plot_max_x:
         ax.axvline(x=0, color='k')
     if draw_x_axis:
         ax.axhline(y=0, color='k')
-    # set labels, enable legend and show
+
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
     plt.show()
-
-    # save in a png file
-    fig.savefig('plot.png')
 
 
 if __name__ == '__main__':
