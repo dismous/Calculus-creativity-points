b35a7430e1f8a9a268e6cdd762da27d9d5f7a95d
dismous <bohdanpopov9@gmail.com>
2023-12-16 22:39:30 +0200

    Update taylor_calculator_and_plotter.py

--- taylor_calculator_and_plotter.py
+++ taylor_calculator_and_plotter.py
@@ -1,50 +1,44 @@
 """
-contains the function which will calculate the nth taylor series
-for a given function and plot it with matplotlib.
+This module contains a function that calculates the nth Taylor series
+for a given function and plots it using matplotlib.
 """
 
-import math as m
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 from sympy import *
 
-x = symbols('x')
+# Define the symbol for sympy
+symbol_x = symbols('x')
 
 
-def calculate_and_plot(development_point: float = 8,
+def calculate_and_plot(base_expr=cos(symbol_x),
+                       development_point: float = 8,
                        grade: int = 4,
-                       base_expr=cos(x),
                        plot_range: int = 4,
                        draw_x_axis: bool = True):
-    """the main function of this module. calculates the nth taylor series"""
-    plot_min_x, plot_max_x = development_point - plot_range, development_point + plot_range
-    plot_points = 1000
-    base_taylor_expr = Float(lambdify(x, base_expr)(development_point))
-    current_function = base_expr
-
-    for k in range(1, grade + 1):
-        derivative_k = Derivative(current_function, x).doit()
-        derivative_k_solved = lambdify(x, derivative_k)(development_point)
-
-        base_taylor_expr += (derivative_k_solved / m.factorial(k)) * ((x - development_point) ** k)
-
-        current_function = derivative_k
+    """Calculate the nth Taylor series and plot it."""
+    min_x, max_x = development_point - plot_range, development_point + plot_range
+    num_points = 1000
+    taylor_expr = Float(lambdify(symbol_x, base_expr)(development_point))
+    current_expr = base_expr
+    for i in range(1, grade + 1):
+        derivative = Derivative(current_expr, symbol_x).doit()
+        derivative_at_point = lambdify(symbol_x, derivative)(development_point)
+        taylor_expr += (derivative_at_point / math.factorial(i)) * ((symbol_x - development_point) ** i)
+        current_expr = derivative
 
     print('f(x) = ' + str(base_expr))
-    print(f'T({grade}, {development_point})(x) = ' + str(base_taylor_expr))
-
-    lam_base_expr = lambdify(x, base_expr, modules=['numpy'])
-    lam_taylor_expr = lambdify(x, base_taylor_expr, modules=['numpy'])
-
-    x_vals = np.linspace(plot_min_x, plot_max_x, plot_points)
-    y_vals_base_expr, y_vals_taylor_expr = lam_base_expr(x_vals), lam_taylor_expr(x_vals)
-
+    print(f'T({grade}, {development_point})(x) = ' + str(taylor_expr))
+    base_func = lambdify(symbol_x, base_expr, modules=['numpy'])
+    taylor_func = lambdify(symbol_x, taylor_expr, modules=['numpy'])
+    x_values = np.linspace(min_x, max_x, num_points)
+    y_values_base, y_values_taylor = base_func(x_values), taylor_func(x_values)
     fig, ax = plt.subplots()
-    ax.plot(x_vals, y_vals_base_expr, label=str(base_expr))
-    ax.plot(x_vals, y_vals_taylor_expr, label=f'T({grade}, {development_point})(x)')
+    ax.plot(x_values, y_values_base, label=str(base_expr))
+    ax.plot(x_values, y_values_taylor, label=f'T({grade}, {development_point})(x)')
     ax.grid(True, which='both')
-
-    if plot_min_x < 0 < plot_max_x:
+    if min_x < 0 < max_x:
         ax.axvline(x=0, color='k')
     if draw_x_axis:
         ax.axhline(y=0, color='k')
